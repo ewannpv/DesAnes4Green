84625ed975a7824889bda520822d72e19edd94e8
Ewann <ewannpv@gmail.com>
2021-11-17 12:10:06 +0100

    Removes models

--- src/models/AdminLeftPanelEnum.ts
+++
@@ -1,7 +0,0 @@
-const AdminLeftPanelEnum = Object.freeze({
-  HOME: 0,
-  ADD_COURSE: 1,
-  QUIT_PANEL: 2,
-});
-
-export default AdminLeftPanelEnum;
--- src/models/topNavBarEnum.ts
+++
@@ -1,7 +0,0 @@
-const TopNavBarEnum = Object.freeze({
-  HOME: 0,
-  ADMIN_PANEL: 1,
-  LOGOUT: 2,
-});
-
-export default TopNavBarEnum;
