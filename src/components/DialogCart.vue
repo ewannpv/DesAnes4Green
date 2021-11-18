9aad3fd73c253cb8cc70ea98ce8d2f4495061b2d
Ewann <ewannpv@gmail.com>
2021-11-18 21:38:41 +0100

    Updates upload

--- src/components/DialogCart.vue
+++ src/components/DialogCart.vue
@@ -1,6 +1,6 @@
 <template>
   <v-dialog :value="dialogCart" @click:outside="closeDialog()" max-width="70%">
-    <div ref="dialogRef">
+    <div>
       <v-card color="grey lighten-4">
         <v-card-title>
           <span class="text-h4" color="primary" dark>Phases du projet</span>
@@ -52,16 +52,77 @@
         </v-card-actions>
       </v-card>
     </div>
+    <div class="d-none">
+      <VueHtml2pdf :manual-pagination="true" :enable-download="true" ref="dialogRef">
+        <section slot="pdf-content">
+          <div ref="dialogRef">
+            <v-card color="grey lighten-4">
+              <v-card-title>
+                <span class="text-h4" color="primary" dark>Phases du projet</span>
+              </v-card-title>
+              <v-col cols="12">
+                <v-row>
+                  <v-col v-for="item in itemList" :key="item[0]" class="" cols="12" sm="12">
+                    <span class="text-h5 ml-3">{{ item[0] }}</span>
+                    <v-col v-for="child in item[1]" :key="child.ID" class="" cols="12" sm="12">
+                      <v-card>
+                        <v-col>
+                          <v-row class="ma-2 pb-2">
+                            <v-chip text-color="white" label color="primary">
+                              {{ child.ID }}</v-chip
+                            >
+                          </v-row>
+                          <v-row class="ma-2"><b>Critère:</b>&ensp;{{ child.Criteres }}</v-row>
+                          <v-row class="ma-2"><b>Tests:</b></v-row>
+                          <v-row class="ma-2"
+                            ><ul>
+                              <li class="ma-2" v-for="(test, index) in child.Tests" :key="index">
+                                {{ test }}
+                              </li>
+                            </ul></v-row
+                          >
+                          <v-row class="ma-2"
+                            ><b>Indicateurs:</b>&ensp;
+                            {{ child.Indicateurs ? child.Indicateurs : 'N/A' }}</v-row
+                          >
+                          <v-row class="ma-2"
+                            ><ul>
+                              <li class="ma-2">
+                                <b>X:</b>&ensp;{{ child.Xindicateur ? child.Xindicateur : 'N/A' }}
+                              </li>
+                              <li class="ma-2">
+                                <b>Y:</b>&ensp;{{ child.Xindicateur ? child.Yindicateur : 'N/A' }}
+                              </li>
+                            </ul></v-row
+                          >
+                        </v-col>
+                      </v-card>
+                    </v-col>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-card>
+          </div>
+        </section>
+      </VueHtml2pdf>
+    </div>
   </v-dialog>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import VueHtml2pdf from 'vue-html2pdf';
 import store from '@/store';
 import { Item } from '@/interfaces/item';
 import * as api from '../api';
 
 export default Vue.extend({
+  data(): unknown {
+    return {
+      downloading: false,
+    };
+  },
+  components: { VueHtml2pdf },
   computed: {
     dialogCart(): boolean {
       return store.getters.DIALOG_CART;
@@ -90,14 +151,9 @@ export default Vue.extend({
       store.dispatch('UPDATE_DIALOG_CART', false);
     },
     downloadCart(): void {
-      const html = this.$refs.dialogRef as HTMLElement;
-
-      const w = window.open() as Window;
-      w.document.write(html.innerHTML);
-      w.document.close();
-      w.setTimeout(() => {
-        w.print();
-      }, 1000);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const html = this.$refs.dialogRef as any;
+      html.generatePdf();
     },
   },
 });
