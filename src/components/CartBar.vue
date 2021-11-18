0478891851f7d8cf10207be0b2f8573528b9f23e
Ewann <ewannpv@google.com>
2021-11-18 11:39:41 +0100

    Updates cart

--- src/components/CartBar.vue
+++ src/components/CartBar.vue
@@ -1,35 +1,60 @@
 <template>
   <v-container fluid pt-5 mt-2>
     <h2 class="ml-3 pt-3">Panier {{ count }}</h2>
-
-    <v-list dense>
-      <v-list-item v-for="(item, i) in items" :key="i">
-        <v-list-item-content>
-          <v-list-item-title v-text="item.ID"></v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-btn icon>
-            <v-icon color="grey lighten-1">mdi-delete</v-icon>
-          </v-btn>
-        </v-list-item-action>
-      </v-list-item>
+    <v-list>
+      <v-list-group
+        v-for="listItem in listGroup"
+        :key="listItem.title"
+        v-model="listItem.active"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="listItem.title"></v-list-item-title>
+            <v-list-item-subtitle v-text="listItem.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="child in listItem.items" :key="child.title">
+          <v-list-item-content>
+            <v-list-item-title v-text="child.ID"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
     </v-list>
   </v-container>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
 import store from '@/store';
 import { Item } from '@/interfaces/item';
 
-export default {
+export default Vue.extend({
   computed: {
     // List of selected items.
     items(): Item[] {
       return store.getters.SELECTED_ITEMS;
     },
+    necessaryItems(): Item[] {
+      return store.getters.NECESSARY_ITEMS;
+    },
     count(): string {
       return ` (${store.getters.CART_LEN})`;
     },
+    listGroup() {
+      return [
+        {
+          items: store.getters.NECESSARY_ITEMS,
+          title: 'Incontournables',
+          subtitle: `Elements: ${store.getters.NECESSARY_ITEMS.length}`,
+        },
+        {
+          items: store.getters.SELECTED_ITEMS,
+          title: 'Autres',
+          subtitle: `Elements: ${store.getters.SELECTED_ITEMS.length}`,
+        },
+      ];
+    },
   },
-};
+});
 </script>
