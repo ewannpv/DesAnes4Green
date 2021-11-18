c72a685db45d36aeb470953a15d89f384b4da312
Ewann <ewannpv@google.com>
2021-11-18 10:31:22 +0100

    Updates cart

--- src/components/CartBar.vue
+++ src/components/CartBar.vue
@@ -1,16 +1,19 @@
 <template>
   <v-container fluid pt-5 mt-2>
     <h2 class="ml-3 pt-3">Panier {{ count }}</h2>
-    <v-list-item v-for="item in items" :key="item.ID" class="mb-2 mt-2">
-      <v-list-item-content>
-        <v-list-item-title> {{ item.ID }}</v-list-item-title>
-      </v-list-item-content>
-      <v-list-item-action>
-        <v-btn icon>
-          <v-icon color="grey lighten-1">mdi-delete</v-icon>
-        </v-btn>
-      </v-list-item-action>
-    </v-list-item>
+
+    <v-list dense>
+      <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item-content>
+          <v-list-item-title v-text="item.ID"></v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon>
+            <v-icon color="grey lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
