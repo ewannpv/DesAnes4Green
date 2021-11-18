50c491741a064d8107acd8477fd1e93860f3d47b
Ewann <ewannpv@gmail.com>
2021-11-18 20:53:28 +0100

    rebasing

--- src/components/DialogCart.vue
+++ src/components/DialogCart.vue
@@ -1,56 +1,58 @@
 <template>
   <v-dialog :value="dialogCart" @click:outside="closeDialog()" max-width="70%">
-    <v-card>
-      <v-card-title>
-        <span class="text-h4">Phases du projet</span>
-      </v-card-title>
-      <v-col cols="12">
-        <v-row>
-          <v-col v-for="item in itemList" :key="item[0]" class="" cols="12" sm="12">
-            <v-card color="green lighten-4 mt-3">
-              <v-card-title primary-title> {{ item[0] }} </v-card-title>
-              <v-col v-for="child in item[1]" :key="child.ID" class="" cols="12" sm="12">
-                <v-card>
-                  <v-col>
-                    <v-row class="ma-2 pb-2">
-                      <v-chip text-color="white" label color="primary"> {{ child.ID }}</v-chip>
-                    </v-row>
-                    <v-row class="ma-2"><b>Critère:</b>&ensp;{{ child.Criteres }}</v-row>
-                    <v-row class="ma-2"><b>Tests:</b></v-row>
-                    <v-row class="ma-2"
-                      ><ul>
-                        <li class="ma-2" v-for="(test, index) in child.Tests" :key="index">
-                          {{ test }}
-                        </li>
-                      </ul></v-row
-                    >
-                    <v-row class="ma-2"
-                      ><b>Indicateurs:</b>&ensp;
-                      {{ child.Indicateurs ? child.Indicateurs : 'N/A' }}</v-row
-                    >
-                    <v-row class="ma-2"
-                      ><ul>
-                        <li class="ma-2">
-                          <b>X:</b>&ensp;{{ child.Xindicateur ? child.Xindicateur : 'N/A' }}
-                        </li>
-                        <li class="ma-2">
-                          <b>Y:</b>&ensp;{{ child.Xindicateur ? child.Yindicateur : 'N/A' }}
-                        </li>
-                      </ul></v-row
-                    >
-                  </v-col>
-                </v-card>
-              </v-col>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text color="green darken-2" @click="downloadCart()">Telecharger</v-btn>
-        <v-btn text color="green darken-2" @click="closeDialog()">Fermer</v-btn>
-      </v-card-actions>
-    </v-card>
+    <div ref="dialogRef">
+      <v-card>
+        <v-card-title>
+          <span class="text-h4">Phases du projet</span>
+        </v-card-title>
+        <v-col cols="12">
+          <v-row>
+            <v-col v-for="item in itemList" :key="item[0]" class="" cols="12" sm="12">
+              <v-card color="green lighten-4 mt-3">
+                <v-card-title primary-title> {{ item[0] }} </v-card-title>
+                <v-col v-for="child in item[1]" :key="child.ID" class="" cols="12" sm="12">
+                  <v-card>
+                    <v-col>
+                      <v-row class="ma-2 pb-2">
+                        <v-chip text-color="white" label color="primary"> {{ child.ID }}</v-chip>
+                      </v-row>
+                      <v-row class="ma-2"><b>Critère:</b>&ensp;{{ child.Criteres }}</v-row>
+                      <v-row class="ma-2"><b>Tests:</b></v-row>
+                      <v-row class="ma-2"
+                        ><ul>
+                          <li class="ma-2" v-for="(test, index) in child.Tests" :key="index">
+                            {{ test }}
+                          </li>
+                        </ul></v-row
+                      >
+                      <v-row class="ma-2"
+                        ><b>Indicateurs:</b>&ensp;
+                        {{ child.Indicateurs ? child.Indicateurs : 'N/A' }}</v-row
+                      >
+                      <v-row class="ma-2"
+                        ><ul>
+                          <li class="ma-2">
+                            <b>X:</b>&ensp;{{ child.Xindicateur ? child.Xindicateur : 'N/A' }}
+                          </li>
+                          <li class="ma-2">
+                            <b>Y:</b>&ensp;{{ child.Xindicateur ? child.Yindicateur : 'N/A' }}
+                          </li>
+                        </ul></v-row
+                      >
+                    </v-col>
+                  </v-card>
+                </v-col>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text color="green darken-2" @click="downloadCart()">Telecharger</v-btn>
+          <v-btn text color="green darken-2" @click="closeDialog()">Fermer</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </v-dialog>
 </template>
 
@@ -96,7 +98,7 @@ export default Vue.extend({
       w.document.close();
       w.setTimeout(() => {
         w.print();
-      }, 500);
+      }, 1000);
     },
   },
 });
